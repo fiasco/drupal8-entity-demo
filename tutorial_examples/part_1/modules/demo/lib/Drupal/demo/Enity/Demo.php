8d7b88b55bf7b6b7bbccf4e8b2761ca242c19501
Josh Waihi <josh.wahi@acquia.com>
2014-05-10 10:09:03 +1200

    Part 1: Lets Create a Config Entity module

---
+++ tutorial_examples/part_1/modules/demo/lib/Drupal/demo/Enity/Demo.php
@@ -0,0 +1,28 @@
+<?php
+namespace Drupal\demo\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+
+/**
+ * Defines a Block configuration entity class.
+ *
+ * @ConfigEntityType(
+ *   id = "droplet",
+ *   label = @Translation("Droplet")
+ * )
+ */
+class Demo extends ConfigEntityBase {
+    /**
+     * The Entity label.
+     *
+     * @var string
+     */
+    public $label;
+
+    /**
+     * The Entity machine name.
+     *
+     * @var string
+     */
+    public $id;
+}
